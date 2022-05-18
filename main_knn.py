2886b5ca9b2914c8a8a353a732685408ea5aead4
tarun <krishntarun7@gmail.com>
2022-05-18 14:16:21 +0100

    initial

---
+++ main_knn.py
@@ -0,0 +1,271 @@
+# Copyright 2021 solo-learn development team.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import json
+import os
+from pathlib import Path
+import time
+from typing import Tuple
+from xmlrpc.client import Boolean
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from solo.args.setup import parse_args_knn
+from solo.methods import METHODS
+from solo.utils.metrics import AverageMeter, analyse_flops
+
+from solo.utils.classification_dataloader import (
+    prepare_dataloaders,
+    prepare_datasets,
+    prepare_transforms,
+)
+from solo.utils.knn import WeightedKNNClassifier
+
+
+@torch.no_grad()
+def extract_features(loader: DataLoader, model: nn.Module, flag: Boolean) -> Tuple[torch.Tensor]:
+    """Extract features from a data loader using a model.
+
+    Args:
+        loader (DataLoader): dataloader for a dataset.
+        model (nn.Module): torch module used to extract features.
+
+    Returns:
+        Tuple(torch.Tensor): tuple containing the backbone features, projector features and labels.
+    """
+
+    model.eval()
+    block_flops = AverageMeter()
+    
+    backbone_features, proj_features, labels = [], [], []
+    for im, lab in tqdm(loader):
+        im = im.cuda(non_blocking=True)
+        lab = lab.cuda(non_blocking=True)
+        outs = model(im)
+        if flag:
+            batch_size = im.size()[0]
+            # print(batch_size)
+            flops_real = outs["flops_real"]
+            flops_mask = outs["flops_mask"]
+            flops_ori  = outs["flops_ori"]
+            flops_conv, flops_mask, flops_ori, flops_conv1, flops_fc = analyse_flops(
+                                              flops_real, flops_mask, flops_ori, batch_size)
+            block_flops.update(flops_conv, batch_size)
+            # pass
+        backbone_features.append(outs["feats"].detach())
+        proj_features.append(outs["z"])
+        labels.append(lab)
+    model.train()
+    backbone_features = torch.cat(backbone_features)
+    proj_features = torch.cat(proj_features)
+    labels = torch.cat(labels)
+    if flag:
+        model.backbone.record_flops(block_flops.avg, flops_mask, flops_ori, flops_conv1, flops_fc)
+        flops = (block_flops.avg[-1]+flops_mask[-1]+flops_conv1.mean()+flops_fc.mean())/1024
+        flops_per = (block_flops.avg[-1]+flops_mask[-1]+flops_conv1.mean()+flops_fc.mean())/(
+                         flops_ori[-1]+flops_conv1.mean()+flops_fc.mean())*100
+        print(flops.item(), flops_per.item())
+        # pass
+    return backbone_features, proj_features, labels
+
+
+@torch.no_grad()
+def run_knn(
+    train_features: torch.Tensor,
+    train_targets: torch.Tensor,
+    test_features: torch.Tensor,
+    test_targets: torch.Tensor,
+    k: int,
+    T: float,
+    distance_fx: str,
+) -> Tuple[float]:
+    """Runs offline knn on a train and a test dataset.
+
+    Args:
+        train_features (torch.Tensor, optional): train features.
+        train_targets (torch.Tensor, optional): train targets.
+        test_features (torch.Tensor, optional): test features.
+        test_targets (torch.Tensor, optional): test targets.
+        k (int): number of neighbors.
+        T (float): temperature for the exponential. Only used with cosine
+            distance.
+        distance_fx (str): distance function.
+
+    Returns:
+        Tuple[float]: tuple containing the the knn acc@1 and acc@5 for the model.
+    """
+
+    # build knn
+    knn = WeightedKNNClassifier(
+        k=k,
+        T=T,
+        distance_fx=distance_fx,
+    )
+
+    # add features
+    knn(
+        train_features=train_features,
+        train_targets=train_targets,
+        test_features=test_features,
+        test_targets=test_targets,
+    )
+
+    # compute
+    acc1, acc5 = knn.compute()
+
+    # free up memory
+    del knn
+
+    return acc1, acc5
+
+
+def main():
+    args = parse_args_knn()
+    t1 = time.time()
+    
+    print(args.pretrained_checkpoint_dir)
+    if not os.path.isdir(args.pretrained_checkpoint_dir):
+        print("No such checkpoint folder")
+        exit()
+
+    # build paths
+    ckpt_dir = Path(args.pretrained_checkpoint_dir)
+    # print(ckpt_dir, len(os.listdir(ckpt_dir)))
+    args_path = ckpt_dir / "args.json"
+
+    N = len(os.listdir(ckpt_dir)) - 1 #remove json
+    print(N)
+    
+    # ckpt_path = [os.path.join(ckpt_dir / f'epoch={str(i)}.ckpt') for i in range(N)] #ordered list!!
+    # print(ckpt_path)
+    
+    # NOTE neglect these for moments
+    ckpt_path = [os.path.join(ckpt_dir,ckpt) for ckpt in os.listdir(ckpt_dir) if ckpt.endswith(".ckpt")]
+    print(ckpt_path)    
+    # # load arguments
+    if os.path.isfile(args_path):
+        with open(args_path) as f:
+            method_args = json.load(f)
+
+    else:
+        exit()
+    
+
+    # create a json to store results as knn.json
+    if os.path.isfile("./knn.json"):
+        with open("./knn.json") as f:
+            print("Loading json")
+            knn = json.load(f)
+    else:
+        knn = {}
+    
+    print("model width: {}".format(method_args["width"]))
+    print(method_args["den_target"])
+    print("=======================================")
+    try:
+        # print("hello")
+        _ = knn[str(method_args["den_target"])]
+        print("exist")
+    except :
+       
+        knn[str(method_args["den_target"])] = {}
+    print("--------------------------------------")
+    
+    # prepare data
+    # #TODO may take this out of the loop
+    _, T = prepare_transforms(args.dataset)
+    train_dataset, val_dataset = prepare_datasets(
+        args.dataset,
+        T_train=T,
+        T_val=T,
+        data_dir=args.data_dir,
+        train_dir=args.train_dir,
+        val_dir=args.val_dir,
+        download=True,
+    )
+    train_loader, val_loader = prepare_dataloaders(
+        train_dataset,
+        val_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
+    
+    
+    # build the model
+    for md_pth in ckpt_path: #iter over the list
+
+        print("Loading {}".format(md_pth))
+        eph = md_pth.split("/")[-1]
+        # print(eph)
+        model = METHODS[method_args["method"]].load_from_checkpoint(
+            md_pth, strict=False, **method_args
+        )
+        # print(model)
+        model.cuda()
+        print(args.data_dir,args.train_dir, args.val_dir )
+        
+
+        # extract train features
+        train_features_bb, train_features_proj, train_targets = extract_features(train_loader, model, flag=False)
+        train_features = {"backbone": train_features_bb, "projector": train_features_proj}
+
+        del train_features_proj
+
+        # extract test features
+        test_features_bb, test_features_proj, test_targets = extract_features(val_loader, model, flag=True)
+        test_features = {"backbone": test_features_bb, "projector": test_features_proj}
+        
+        del test_features_proj
+        #NOTE running single iteration saving result in json might change for all combinations
+        # run k-nn for all possible combinations of parameters
+        for feat_type in args.feature_type:
+            print(f"\n### {feat_type.upper()} ###")
+            for k in args.k:
+                for distance_fx in args.distance_function:
+                    temperatures = args.temperature if distance_fx == "cosine" else [None]
+                    for T in temperatures:
+                        print("---")
+                        print(f"Running k-NN with params: distance_fx={distance_fx}, k={k}, T={T}...")
+                        acc1, acc5 = run_knn(
+                            train_features=train_features[feat_type],
+                            train_targets=train_targets,
+                            test_features=test_features[feat_type],
+                            test_targets=test_targets,
+                            k=k,
+                            T=T,
+                            distance_fx=distance_fx,
+                        )
+                        print(f"Result: acc@1={acc1}, acc@5={acc5}")
+                    knn[str(method_args["den_target"])][eph]=acc1
+                    # print(knn)
+
+        del model
+        with open('knn.json', 'w', encoding='utf-8') as f:
+            json.dump(knn, f, ensure_ascii=False, indent=4)
+        torch.cuda.empty_cache()
+
+        # break
+    print("time:{}".format(time.time()-t1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_pretrain_prun.py
@@ -0,0 +1,213 @@
+# Copyright 2021 solo-learn development team.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import os
+from pprint import pprint
+
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+from solo.args.setup import parse_args_gating, parse_args_pretrain
+from solo.methods import METHODS
+from solo.utils.auto_resumer import AutoResumer
+from solo.losses.regularization_channel import*
+
+try:
+    from solo.methods.dali import PretrainABC
+except ImportError as e:
+    print(e)
+    _dali_avaliable = False
+else:
+    _dali_avaliable = True
+   
+
+try:
+    from solo.utils.auto_umap import AutoUMAP
+except ImportError:
+    _umap_available = False
+else:
+    _umap_available = True
+
+import types
+
+from solo.utils.checkpointer import Checkpointer
+from solo.utils.classification_dataloader import prepare_data as prepare_data_classification
+from solo.utils.pretrain_dataloader import (
+    prepare_dataloader,
+    prepare_datasets,
+    prepare_n_crop_transform,
+    prepare_transform,
+)
+
+
+def main():
+    seed_everything(5)
+
+    args = parse_args_gating()
+    
+
+    assert args.method in METHODS, f"Choose from {METHODS.keys()}"
+
+    if args.num_large_crops != 2:
+        assert args.method == "wmse"
+    print("======== SSL Method name: {} =================".format(args.method))
+    MethodClass = METHODS[args.method]
+    
+    # if _dali_avaliable:
+    #    args.dali = True
+
+    if args.dali:
+        print("dali")
+        assert (
+            _dali_avaliable
+        ), "Dali is not currently avaiable, please install it first with [dali]."
+        MethodClass = types.new_class(f"Dali{MethodClass.__name__}", (PretrainABC, MethodClass))
+    
+    #create model backbone and initialize optimizers
+    print('creating model')
+    #TODO check otpimzer
+    model = MethodClass(**args.__dict__)
+
+
+    # pretrain dataloader
+    if not args.dali:
+        # asymmetric augmentations
+        if args.unique_augs > 1:
+            transform = [
+                prepare_transform(args.dataset, **kwargs) for kwargs in args.transform_kwargs
+            ]
+        else:
+            transform = [prepare_transform(args.dataset, **args.transform_kwargs)]
+
+        transform = prepare_n_crop_transform(transform, num_crops_per_aug=args.num_crops_per_aug)
+        if args.debug_augmentations:
+            print("Transforms:")
+            pprint(transform)
+
+        train_dataset = prepare_datasets(
+            args.dataset,
+            transform,
+            data_dir=args.data_dir,
+            train_dir=args.train_dir,
+            no_labels=args.no_labels,
+        )
+        train_loader = prepare_dataloader(
+            train_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+        )
+
+    # normal dataloader for when it is available
+    if args.dataset == "custom" and (args.no_labels or args.val_dir is None):
+        val_loader = None
+    elif args.dataset in ["imagenet100", "imagenet"] and args.val_dir is None:
+        val_loader = None
+    else:
+        _, val_loader = prepare_data_classification(
+            args.dataset,
+            data_dir=args.data_dir,
+            train_dir=args.train_dir,
+            val_dir=args.val_dir,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+        )
+
+    
+    # ---------repeat it for each pruning rate---------
+    callbacks = []
+    
+    # wandb logging
+    print(args.wandb)
+    print(str(args.den_target), args.name)
+    if args.wandb:
+        wandb_logger = WandbLogger(
+            name=args.dataset+ "_" + args.name + "_"+ str(args.den_target),
+            # name=args.name + "_"+ str(args.width),
+            project=args.project,
+            entity="tkrishna",
+            offline=args.offline,
+        )
+        wandb_logger.watch(model, log="gradients", log_freq=100)
+        wandb_logger.log_hyperparams(args)
+
+        # lr logging
+        lr_monitor = LearningRateMonitor(logging_interval="epoch")
+        callbacks.append(lr_monitor)
+
+
+    if args.save_checkpoint:
+        # save checkpoint on last epoch only
+        ckpt = Checkpointer(
+            args,
+            logdir=os.path.join(args.checkpoint_dir, args.method),
+            keep_previous_checkpoints=False #for removing prevoius checkpoint
+        )
+        callbacks.append(ckpt)
+
+    print(args.checkpoint_dir, args.method)
+
+    if args.auto_umap:
+        assert (
+            _umap_available
+        ), "UMAP is not currently avaiable, please install it first with [umap]."
+        auto_umap = AutoUMAP(
+            args,
+            logdir=os.path.join(args.auto_umap_dir, args.method),
+            frequency=args.auto_umap_frequency,
+        )
+        callbacks.append(auto_umap)
+
+    # 1.7 will deprecate resume_from_checkpoint, but for the moment
+    # the argument is the same, but we need to pass it as ckpt_path to trainer.fit
+    ckpt_path = None
+    # print(os.path.join(args.checkpoint_dir, args.method))
+    if args.auto_resume and args.resume_from_checkpoint is None:
+        print(os.path.join(args.checkpoint_dir, args.method))
+        auto_resumer = AutoResumer(
+            checkpoint_dir=os.path.join(args.checkpoint_dir, args.method),
+            max_hours=args.auto_resumer_max_hours,
+        )
+        resume_from_checkpoint = auto_resumer.find_checkpoint(args)
+        if resume_from_checkpoint is not None:
+            print(
+                "Resuming from previous checkpoint that matches specifications:",
+                f"'{resume_from_checkpoint}'",
+            )
+            ckpt_path = resume_from_checkpoint
+
+    elif args.resume_from_checkpoint is not None:
+        ckpt_path = args.resume_from_checkpoint
+        del args.resume_from_checkpoint
+
+    trainer = Trainer.from_argparse_args(
+        args,
+        logger=wandb_logger if args.wandb else None,
+        callbacks=callbacks,
+        enable_checkpointing=False,
+    )
+
+    if args.dali:
+        trainer.fit(model,  val_dataloaders=val_loader, ckpt_path=ckpt_path)
+    else:
+        trainer.fit(model, train_loader, val_loader, ckpt_path=ckpt_path)
+    
+    
+        
+
+if __name__ == "__main__":
+    main()
